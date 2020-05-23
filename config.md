6e0d1e65bc57294d58cf7c532a2d89e583c71ebf
Tyler Beason <tbeas12@gmail.com>
2020-05-23 10:50:39 -0700

    full switch from fc-testing to here. local serve works.

---
+++ config.md
@@ -0,0 +1,6 @@
+@def author = "Tyler Beason"
+@def prepath= "financeconferences"
+
+@def div_content = "container-xl"
+
+
---
+++ utils.jl
@@ -0,0 +1,32 @@
+using DelimitedFiles
+
+function hfun_btable(params)
+    path = params[1]
+    csvcontent,headers   = readdlm(path, ',', String, header=true)
+    nrows, ncols = size(csvcontent)
+    io = IOBuffer()
+    # write headers
+    println(io,"<table class=\"table table-striped table-responsive table-sm\">")
+    println(io,"<caption>","$nrows conferences","</caption>")
+    println(io,"<thead class=\"thead-dark\">")
+    println(io,"<tr>")
+    for i in 1:ncols-1
+        println(io,"<th scope=\"col\" class=\"align-middle\">",headers[i],"</th>")
+    end
+    println(io,"</tr>")
+    println(io,"</thead>")
+
+    # writing content
+    println(io,"<tbody>")
+    for i in 1:nrows
+        println(io, "<tr>")
+
+        println(io,"<td>","<a href=\"",csvcontent[i,ncols],"\">",csvcontent[i,1],"</a>","</td>")
+        for j in 2:ncols-1
+            println(io,"<td>",csvcontent[i,j],"</td>")
+        end
+        println(io,"</tr>")
+    end
+    println(io,"</table>")
+    return String(take!(io))
+end
